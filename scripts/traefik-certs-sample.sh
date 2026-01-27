56264c67f2ddd06ed9fbff9234bab646466f69d9
Kyle Rafaels <kylerafaels@fico.com>
2026-01-27 12:27:00 -0600

    adding new argo install method, traefik and cert creation

---
+++ scripts/traefik-certs-sample.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# 1) Generate a self‑signed certificate valid for *.docker.localhost
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout tls.key -out tls.crt \
+  -subj "/CN=*.docker.localhost"
+
+# 2) Create the TLS secret in the traefik namespace
+kubectl create secret tls local-selfsigned-tls \
+  --cert=tls.crt --key=tls.key \
+  --namespace traefik
